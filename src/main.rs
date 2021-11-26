1d5d3bdec051d7e1bf82954841bc1376eef83f34
Faumaray <siroggi5@gmail.com>
2021-11-26 13:57:20 +0400

    Basic windows TODO: 1) Make Layout 2) Add static files 3) Build.rs file for including static files 4) Prepare file with user guide and teorethical information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![windows_subsystem = "windows"]
+
+use gtk::prelude::*;
+
+use gtk::glib::clone;
+use gtk::glib::signal::Inhibit;
+use gtk::{Application, TextBuffer, TextView};
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        gtk::Application::new(Some("com.github.Faumaray.applied_statistics_ANOVA_gtk_rs"), Default::default());
+    application.connect_activate(build_ui);
+    application.run();
+}
+fn build_ui(application: &Application) {
+    let window = Rc::new(gtk::ApplicationWindow::new(application));
+
+    window.set_title(Some("ANOVA"));
+    window.set_default_size(900, 720);
+    window.set_resizable(false);
+    window.show();
 }
